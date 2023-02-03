964f59b0199b02487347ab4fb0ba9b42067f8569
rihan97 <rihan.alam97@gmail.com>
2023-02-03 20:17:14 +0000

    editing tip file

---
+++ Tip-Calculator/tip-calculator.py
@@ -0,0 +1,10 @@
+print("Welcome, to the tip calculator")
+total_bill = input("What is the total bill? £")
+num_0f_ppl = input("How many poeple are we splitting the bill with? ")
+tip_percentage = input("What percentage tip would you like to give? ")
+
+tip = total_bill * num_0f_ppl
+
+
+print("Each person should pay: £" +tip)
+
